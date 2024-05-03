4cc7d6fc00c973fae862147ca069dc1afad760ec
w1947567 <163130409+w1947567@users.noreply.github.com>
2024-05-03 15:10:20 +0100

    Update Hello.py
    Full code for Streamlit app

--- Hello.py
+++ Hello.py
@@ -1,51 +1,123 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
+import pandas as pd
 import streamlit as st
-from streamlit.logger import get_logger
-
-LOGGER = get_logger(__name__)
-
-
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
-
-    st.write("# Welcome to Streamlit! 👋")
-
-    st.sidebar.success("Select a demo above.")
-
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
+import pip as pip
+from PIL import Image
+import plotly.express as px 
 
 
-if __name__ == "__main__":
-    run()
+# Title of the Dashboard
+st.title("Wastewater Discharges Per Year and Per Country")
+
+# File uploader
+uploaded_file = st.file_uploader("Upload your file", type="xlsx")
+
+if uploaded_file is not None:
+    try:
+        
+        df = pd.read_excel(uploaded_file)
+        
+        # If statement to check if Data frame is not empty
+        if not df.empty:
+            # Present the entire DataFrame
+            st.write("Uploaded DataFrame:")
+            st.write(df)
+            
+            # Display specific columns
+            st.write("Selected Columns:")
+            selected_columns = st.multiselect("Select columns to display", df.columns)
+            if selected_columns:
+                st.write(df[selected_columns])
+            
+            # Columns for display
+            export_columns = st.multiselect("Select columns to export", df.columns)
+            if st.button("Export Selected Columns"):
+                selected_df = df[export_columns]
+                st.write(selected_df)
+        else:
+            st.write("The uploaded file is empty.")
+            
+        # Sidebar widgets for country
+        st.sidebar.header("Select filter:")
+        selected_countries=[]
+        if "Country" in df.columns:
+            selected_countries= st.sidebar.multiselect(
+                "Select the country",
+                options=df["Country"].unique(),
+                default=df["Country"].unique().tolist()
+            )
+        else:
+            st.sidebar.write("Country column not found.")
+            
+        
+        #Sidebar widegts for year:
+        st.sidebar.header("Select filter:")
+        selected_years=[]
+        if "Year" in df.columns:
+            selected_years= st.sidebar.multiselect(
+                "Select the year",
+                options=df["Year"].unique(),
+                default=df["Year"].unique().tolist()
+            )
+        else:
+             st.sidebar.write("Year column not found.")
+             
+        
+        # DataFrame based on selected countries and years
+        filtered_df = df[df["Country"].isin(selected_countries) & df["Year"].isin(selected_years)]
+
+        # MAP for volume of discharges to inland waters
+        if not filtered_df.empty:
+            figure = px.choropleth(filtered_df, 
+                        locations="Country",
+                        locationmode="country names",
+                        color="Total discharges to Inland waters(million m3)",  # Remove extra space
+                        hover_name="Country",
+                        animation_frame="Year",
+                        title="Wastewater Discharges Per Year and Per Country to inland waters (million m3)",
+                        labels={"Total discharges to Inland waters(million m3)": "Total Discharges (million m3)"}
+                        )
+            figure.update_layout(geo=dict(showcoastlines=True))
+            st.plotly_chart(figure)
+        else:
+            st.write("No data available for the selected filters.")
+
+        # MAP for volume of discharges to sea 
+        if not filtered_df.empty:
+            figure = px.choropleth(filtered_df, 
+                        locations="Country",
+                        locationmode="country names",
+                        color="Total discharges to the sea(million m3)",  # Remove extra space
+                        hover_name="Country",
+                        animation_frame="Year",
+                        title="Wastewater Discharges Per Year and Per Country to the sea (million m3)",
+                        labels={"Total discharges to the sea(million m3)": "Total Discharges (million m3)"}
+                       )
+            figure.update_layout(geo=dict(showcoastlines=True))
+            st.plotly_chart(figure)
+        else:
+            st.write("No data available for the selected filters.")
+    
+        # Define sidebar widget for selecting category
+        selected_category = st.multiselect("Select category", 
+                                    ["Agricultural wastewater", "Urban wastewater", "Industrial wastewater"])
+
+        # Plot bar charts based on selected categories
+        for category in selected_category:
+            if category == "Agricultural wastewater":
+                figure = px.bar(df, x='Year', y='Agricultural (incl. forestry + fisheries) wastewater, all sources, direct discharges(million m3)',
+                     title="Agricultural Wastewater Discharges Over the Years",
+                     labels={"Agricultural (incl. forestry + fisheries) wastewater, all sources, direct discharges(million m3)": "Volume (million m3)"})
+                st.plotly_chart(figure)
+            elif category == "Urban wastewater":
+                figure = px.bar(df, x='Year', y='Urban wastewater, all sources, discharged without treatment(million m3)',
+                     title="Urban Wastewater Discharges Over the Years",
+                     labels={"Urban wastewater, all sources, discharged without treatment(million m3)": "Volume (million m3)"})
+                st.plotly_chart(figure)
+            elif category == "Industrial wastewater":
+                figure = px.bar(df, x='Year', y='Industrial wastewater, all sources, discharged without treatment(million m3)',
+                     title="Industrial Wastewater Discharges Over the Years",
+                     labels={"Industrial wastewater, all sources, discharged without treatment(million m3)": "Volume (million m3)"})
+                st.plotly_chart(figure)
+    except Exception as e:
+        st.error(f"An error occurred: {e}")
+
